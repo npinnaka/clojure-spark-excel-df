d313ac191c1cb9512f92a71a40b471225c042d7a
Narendra Pinnaka <npinnaka@yahoo.com>
2018-11-10 08:17:16 -0600

    added clojure project

---
+++ test/clojure_spark_excel_df/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-spark-excel-df.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-spark-excel-df.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
